634af7f9a103096d3f3a7a33f755d13bec072343
Olechka <ifeellikemore@Dmitriys-MacBook-Pro-2.local>
2021-09-26 16:52:44 -0700

    fix issues found during code review

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,37 @@
 let popupWindow = document.querySelector('.popup');
 let popupWindowCloseBtn = popupWindow.querySelector('.popup__close-btn');
 let editBtn = document.querySelector('.profile-info__edit-button');
-
-function togglePopupWindow() {
-  popupWindow.classList.toggle('popup_opened');
-}
-
-editBtn.addEventListener('click', togglePopupWindow);
-popupWindowCloseBtn.addEventListener('click', togglePopupWindow);
-
-function popupOverlayClick(evt) {
-  if (evt.target === evt.currentTarget) {
-    togglePopupWindow();
-  }
-}
-
-popupWindow.addEventListener('click', popupOverlayClick);
-
 let form = document.querySelector('.popup-form');
 let inputName = form.querySelector('#username');
 let inputJob = form.querySelector('#job');
-let submitBtn = form.querySelector('.popup-form__submit-btn');
+let username = document.querySelector('.profile-info__name');
+let job = document.querySelector('.profile-info__job');
+
+function openPopupWindow() {
+  inputName.value = username.textContent;
+  inputJob.value = job.textContent;
+  popupWindow.classList.add('popup_opened');
+}
+
+function closePopupWindow() {
+  popupWindow.classList.remove('popup_opened');
+}
+
+// закомментировано на будущее :) (close pop-up window by clicking on the overlay layer)
+// function popupOverlayClick(evt) {
+//   if (evt.target === evt.currentTarget) {
+//     togglePopupWindow();
+//   }
+// }
+// popupWindow.addEventListener('click', popupOverlayClick);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let inputNameValue = inputName.value;
-  let inputJobValue = inputJob.value;
-  let username = document.querySelector('.profile-info__name');
-  let job = document.querySelector('.profile-info__job');
-  username.textContent = `${inputNameValue}`;
-  job.textContent = `${inputJobValue}`;
+  username.textContent = inputName.value;
+  job.textContent = inputJob.value;
+  closePopupWindow();
 }
 
+editBtn.addEventListener('click', openPopupWindow);
+popupWindowCloseBtn.addEventListener('click', closePopupWindow);
 form.addEventListener('submit', formSubmitHandler);
-submitBtn.addEventListener('click', togglePopupWindow);
